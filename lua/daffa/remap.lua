22c0fb68f7a8841b8892459e8234dce5f147ceea
Daffa Randika <daffa.randika.3@gmail.com>
2022-12-26 08:17:46 +0700

    ctrl + / to comment

--- lua/daffa/remap.lua
+++ lua/daffa/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.g.user_emmet_leader_key=","
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+
+-- auto copy bashrc to bash folder
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("auto copy file to bash folder", {
         clear = true
@@ -10,3 +12,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
         vim.cmd(':!autoCopyBashrc')
     end,
 })
+
+-- ctrl + / to comment
+vim.keymap.set("v", "<C-/>", ":norm 0i//<Esc>")
